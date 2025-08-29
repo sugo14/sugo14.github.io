6d7efaf4c62c3e053a96ec88024b167bbefe9529
sugo14 <98863576+sugo14@users.noreply.github.com>
2025-08-29 14:18:26 -0600

    Fixed huge network usage on sugo gif

--- scripts/sugo.js
+++ scripts/sugo.js
@@ -4,14 +4,23 @@ let currCnt = 0;
 const maxCnt = 12;
 const filePath = "/assets/art/sugo-gif/"
 
+let files = []
+
+async function loadFiles() {
+    for (let i = 0; i <= maxCnt; i++) {
+        const frameFilePath = `${filePath}${i}.txt`;
+        const response = await fetch(frameFilePath);
+        const text = await response.text();
+        files.push(text);
+    }
+}
+
 async function updateGif() {
     currCnt = (currCnt + 1) % (maxCnt + 1);
-    const frameFilePath = `${filePath}${currCnt}.txt`;
-    const response = await fetch(frameFilePath);
-    const text = await response.text();
-    sugoGif.innerHTML = text;
+    sugoGif.innerHTML = files[currCnt];
 }
 
 if (sugoGif) {
+    loadFiles();
     window.setInterval(updateGif, 1000 / 10);
 }
