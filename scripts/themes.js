7dc4e1ef79e74a3fb05b4d7c7b3b5935c970ba6c
sugo14 <98863576+sugo14@users.noreply.github.com>
2025-08-30 19:15:12 -0600

    Added theme support

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,4 +1,13 @@
+const themeCnt = 4;
+
+let currTheme = localStorage.getItem("theme");
+if (currTheme === null) { currTheme = 0; }
+else { currTheme = parseInt(currTheme); }
+document.documentElement.setAttribute("theme", currTheme);
+
 function toggleTheme() {
-    console.log("HI");
+    currTheme = (currTheme + 1) % themeCnt;
+    document.documentElement.setAttribute("theme", currTheme);
+    localStorage.setItem("theme", currTheme);
 }
 window.toggleTheme = toggleTheme;
